01f8f8fe57caec9967d3039aefbd4e413443db9b
jbear18 <League@AdministorsiMac.attlocal.net>
2018-09-03 18:28:04 -0700

    Vault, Constructors Checkpoint, Pig + Candy Checkpoint (IP)

--- src/JamesBond.java
+++ src/JamesBond.java
@@ -1,4 +1,22 @@
 
 public class JamesBond {
-
+	private Vault vault;
+	
+	
+	JamesBond(Vault vault) {
+		this.vault = vault;
+	}
+	
+	
+	int findCode(Vault vault) {
+		
+		for (int i = 0; i < 1000000; i++) {
+			if(vault.tryCode(i)) {
+				return i;
+			}
+		}
+		
+		return 0;
+		
+	}
 }
--- src/VaultRunner.java
+++ src/VaultRunner.java
@@ -1,7 +1,10 @@
 
 public class VaultRunner {
 	public static void main(String[] args) {
-		Vault vault = new Vault(1000000000);
+		Vault vault = new Vault(396582);
+		JamesBond bond = new JamesBond(vault);
 		vault.tryCode(1000000000);
+		System.out.println(vault.tryCode(396582));
+		System.out.println(bond.findCode(vault));
 	}
 }
